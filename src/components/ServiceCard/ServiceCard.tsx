6b245fcc62916106d9ff3d3edf49927e954f5542
Rafael <rafa.scarpel@gmail.com>
2025-09-12 19:59:20 -0300

    style: estilização dos status dos serviços dentro dos cards

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styles from "./ServiceCard.module.css"
 import type { Service } from "../../types";
+import type { ServiceStatus } from "../../types";
 
 interface ServiceCardProps{
     service: Service;
 }
 
 export default function ServiceCard({service}: ServiceCardProps){
+
+    const statusClasses: Record<ServiceStatus, string>  = {
+        operational: styles.operational,
+        degraded: styles.degraded,
+        outage: styles.outage
+    };
+
+    const statusClass = statusClasses[service.status.toLowerCase() as ServiceStatus] || '';
+
     return(
         <div className={styles.card}>
             <h2 className={styles.name}>{service.name}</h2>
-            <div>
+            <div className={`${styles.statusContainer} ${statusClass}`}>
                 <p className={styles.status}>{service.status}</p>
             </div>
         </div>

3fb8f446d4088f3faa8fde6a279a6ffa9db75594
Rafael <rafa.scarpel@gmail.com>
2025-09-12 12:47:51 -0300

    feat: hook personalizado para serviços

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,24 +1,33 @@
-import { mockServices } from "../../data/mock-services";
 import ServiceCard from "../ServiceCard/ServiceCard";
 import styles from "./Dashboard.module.css";
 import { Search } from "lucide-react";
+import { useServiceStatus } from "../../hooks/useServiceStatus";
 
 export default function Dashboard() {
+  const { services, isLoading, error } = useServiceStatus();
+
   return (
     <div className={styles.container}>
       <div className={styles.barraPesquisa}>
-        <input type="text" placeholder="Pesquisar..." className={styles.input} />
+        <input
+          type="text"
+          placeholder="Pesquisar..."
+          className={styles.input}
+        />
         <span>
           <Search />
         </span>
       </div>
 
       <div className={styles.cardsContainer}>
-      {mockServices.map((service) => (
-        <ServiceCard key={service.id} service={service} />
-      ))}
+        {isLoading && <p className={styles.loading}>Carregando serviços...</p>}
+        {error && <p>Erro ao carregar: {error.message}</p>}
+        {!isLoading &&
+          !error &&
+          services.map((service) => (
+            <ServiceCard key={service.id} service={service} />
+          ))}
       </div>
-
     </div>
   );
 }
